9f5accd673020b3cac3a09b8e723e869f29316cc
Stanley Ivan Sandjo <heboivan@gmail.com>
2026-05-10 17:08:41 +0200

    Track bash commands

---
+++ Bash/clear-logs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#? Clear project's logs
+
+PROJECT_DIR="$HOME/Dev/projects/Qargo Unavailability Sync Service"
+
+cd "$PROJECT_DIR" || echo "Project directory not found: \"$PROJECT_DIR\"" && exit 1
+
+trash "$PROJECT_DIR/Logs/"*.log
+
+echo "✅ Project logs have been cleared"
---
+++ Bash/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#? Run the console app
+#? Note: It runs by locking and skipping concurrent executions
+
+PROJECT_DIR="$HOME/Dev/projects/Qargo Unavailability Sync Service"
+
+cd "$PROJECT_DIR" || echo "Project directory not found: \"$PROJECT_DIR\"" && exit 1
+
+echo "Starting project..."
+
+flock -n /tmp/qargo-sync.lock \
+dotnet "$PROJECT_DIR/publish/QargoUnavailabilitySyncService.dll"
+
+echo "✅ Project has run successfully"
